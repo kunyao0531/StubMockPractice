b7103b2d0cf5a954ba7493b2174e94eabb493e84
kunyao0531 <frankwang11@hotmail.com>
2015-03-16 17:07:36 +0800

    Update Dal

--- MainProject/Dal.cs
+++ MainProject/Dal.cs
@@ -12,18 +12,22 @@ namespace MainProject
         {
             if (Name == "Frank")
             {
+                //Select 資料庫
                 return 27;
             }
             else if (Name == "Tom")
             {
+                //Select 資料庫
                 return 88;
             }
             else if (Name == "Kevin")
             {
+                //Select 資料庫
                 return 15;
             }
             else
             {
+                //Select 資料庫
                 return 99;
             }
         }
